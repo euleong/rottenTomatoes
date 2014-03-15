6804e173ce5f72631f3c287f5867bdaf66184461
euleong <el@eugenialeong.com>
2014-03-14 23:48:58 -0700

    Initial Commit

---
+++ rottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes
+//
+//  Created by Eugenia Leong on 3/14/14.
+//  Copyright (c) 2014 Eugenia Leong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
